17f25070fb68de60e83277f8f35d53860b0db749
Matthew Myers <mym3491@yahoo.com.au>
2019-12-03 17:14:42 +1100

    updated roles

--- consul/molecule/default/INSTALL.rst
+++ consul/molecule/default/INSTALL.rst
@@ -1,17 +1,22 @@
 *******
-Vagrant driver installation guide
+Docker driver installation guide
 *******
 
 Requirements
 ============
 
-* Vagrant
-* Virtualbox, Parallels, VMware Fusion, VMware Workstation or VMware Desktop
-* python-vagrant
+* Docker Engine
 
 Install
 =======
 
+Please refer to the `Virtual environment`_ documentation for installation best
+practices. If not using a virtual environment, please consider passing the
+widely recommended `'--user' flag`_ when invoking ``pip``.
+
+.. _Virtual environment: https://virtualenv.pypa.io/en/latest/
+.. _'--user' flag: https://packaging.python.org/tutorials/installing-packages/#installing-to-the-user-site
+
 .. code-block:: bash
 
-    $ sudo pip install python-vagrant
+    $ pip install 'molecule[docker]'
--- consul/molecule/default/tests/test_default.py
+++ consul/molecule/default/tests/test_default.py
@@ -1,23 +1,55 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_host('vagrant')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+user = 'consul_user'
+group = 'consul_group'
+dir = 'consul_dir'
+ex_file = 'bin/consul'
+cfg = 'consul_config'
+cfg_file = 'config.json'
+service_name = 'consul'
+
+@pytest.fixture(scope='module')
+def ans_var(host):
+  return host.ansible(
+    'include_vars', '../../defaults/main.yml'
+  )['ansible_facts']
 
 
-def test_consul_is_installed(File):
-    consul = File("/usr/local/bin/consul")
-    assert consul.exists
+def test_group(host, ans_var):
+    test_group = host.group(ans_var[group])
+    assert test_group.exists
 
 
-def test_consul_template(File):
-    f = File("/etc/consul.d/config.json")
-    assert f.exists
-    assert f.is_file
+def test_user(host, ans_var):
+    test_user = host.user(ans_var[user])
+    assert test_user.exists
 
 
-def test_consul_directory(File):
-    f = File("/tmp/consul")
-    assert f.exists
-    assert f.is_directory
+def test_directory(host, ans_var):
+    test_dir = host.file(ans_var[dir])
+    assert test_dir.exists
+    assert test_dir.is_directory
+
+
+def test_file_installed(host, ans_var):
+    test_file = host.file(ans_var[dir] + '/' + ex_file)
+    assert test_file.exists
+    assert test_file.user == ans_var[user]
+    assert test_file.group == ans_var[group]
+
+
+def test_template(host, ans_var):
+    test_cfg = host.file(ans_var[cfg] + '/' + cfg_file)
+    assert test_cfg.exists
+    assert test_cfg.is_file
+
+
+def test_consul_service(host):
+    service = host.service(service_name)
+    assert service.is_running
+    assert service.is_enabled
